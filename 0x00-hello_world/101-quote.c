86c9b698e861776edbdb057bb3bf35e6b382508f
Richard Msule <msuler@yahoo.com>
2022-09-08 08:41:09 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * main - Prints out the last part of a quote in the standard errors
  *
- * Return: Always 1 (Success)
+ * Return: 1 if success.
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, str1, 59);
-	return (1);
-}
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1); }
