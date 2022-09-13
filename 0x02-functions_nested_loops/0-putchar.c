f227aff4e2af258cd511c56905deefd67053d4df
Richard Msule <msuler@yahoo.com>
2022-09-13 01:55:27 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
