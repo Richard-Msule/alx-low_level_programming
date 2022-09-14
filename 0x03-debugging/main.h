72cce68b19e9bf6fedfadd1923fd5bb330fbe018
Richard Msule <msuler@yahoo.com>
2022-09-14 03:40:41 -0700

    betty bug

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,5 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest number(int a, int b, int c);
-void print remaining days(int month, int day, int year);
-int convert day(int month, int day);
 
 #endif /* MAIN_H */
