4f5abf568260acc84a0e465812b3dbe2762b1aba
Richard Msule <msuler@yahoo.com>
2022-09-15 02:56:59 -0700

    task 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,26 @@
 #include "main.h"
 
-int main(void)
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Description: Can only use _putchar 3 times
+ */
+void more_numbers(void)
 {
-	more_numbers();
-	return (0);
+	int c, i;
+
+	c = 0;
+	while (c < 10)
+	{
+		i = 0;
+		while (i < 15)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+			i++;
+		}
+		c++;
+		_putchar('\n');
+	}
 }
