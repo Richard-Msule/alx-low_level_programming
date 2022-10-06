244e2461c8f0ac66ff2554250542ce9b6d5615dd
Richard Msule <msuler@yahoo.com>
2022-10-06 11:58:47 -0700

    last commit fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,24 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-/* size_t */
-#include <stddef.h>
-
-/* _putchar.c */
-int _putchar(char c);
-
-/* 0-mul.c */
-unsigned int *initDigitArray(size_t size);
-void stringIntMultiply(unsigned int *prod_digits, char *n1_digits,
-				       char *n2_digits, size_t n1_len, size_t n2_len);
-int stringIsPosInt(char *s);
-void error(int status);
-/* int main(int argc, char **argv) */
 
 #endif
